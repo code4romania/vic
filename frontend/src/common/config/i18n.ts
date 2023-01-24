9f1ca79fcedb9549542aee97e4ac03246ecd426d
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2023-01-24 09:48:04 +0200

    chore: add internationalization

---
+++ frontend/src/common/config/i18n.ts
@@ -0,0 +1,19 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import ro from '../../assets/locales/ro/translation.json';
+import en from '../../assets/locales/en/translation.json';
+
+i18n.use(initReactI18next).init({
+  lng: 'ro',
+  fallbackLng: 'en',
+  resources: {
+    ro,
+    en,
+  },
+  debug: true,
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,12 +1,15 @@
 import React, { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
 import { AuthContext } from '../contexts/auth/AuthContext';
 
 const Dashboard = () => {
   const { logout } = useContext(AuthContext);
+  const { t } = useTranslation('general');
 
   return (
     <div>
       This is the Dashboard <button onClick={logout}>logout</button>
+      <h1>Translation test {t('test')}</h1>
     </div>
   );
 };
