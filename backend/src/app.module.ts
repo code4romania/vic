edb27fb55fa3dda90dcc8173b0c03d4d92ef1b8f
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2023-01-26 13:54:26 +0200

    feat: add roles guard

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { APP_GUARD } from '@nestjs/core';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ApiModule } from './api/api.module';
 import { validate } from './common/config/environment-config';
+import { RolesGuard } from './common/guards/roles.guard';
 import { ThrottlerGuardByIP } from './common/guards/throttler.guard';
 import { JsonBodyMiddleware } from './common/middleware/json-body.middlware';
 import { RawBodyMiddleware } from './common/middleware/raw-body.middleware';
@@ -37,6 +38,11 @@ import {
     {
       provide: APP_GUARD,
       useClass: ThrottlerGuardByIP,
+    },
+    {
+      // Global guard for all routes, doesn't require @UseGuards() in each Controller https://docs.nestjs.com/security/authentication#enable-authentication-globally
+      provide: APP_GUARD,
+      useClass: RolesGuard,
     },
   ],
 })
