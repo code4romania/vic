aed44e04288136976b88b2353badad72156aa8e6
Andrew Radulescu <andrew.radulescu@wearetribus.com>
2023-02-01 15:11:11 +0200

    refactor: [TEO-176] Fix ESlint and TSconfig and refactor configuration to disallow 'any' implicit and explicit

--- backend/src/infrastructure/config/logging.config.ts
+++ backend/src/infrastructure/config/logging.config.ts
@@ -1,8 +1,10 @@
+import { Request } from 'express';
+
 export const PinoLoggerConfig = {
   pinoHttp: {
     level: 'trace',
     autoLogging: {
-      ignore: (req) => {
+      ignore: (req: Request): boolean => {
         return req.url.includes('health');
       },
     },
