b1cb932b9fea803cd8b205c455f90bb108659498
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2023-02-02 18:09:47 +0200

    feat(user): add basic jwt web user authentication for user

--- backend/src/api/api.module.ts
+++ backend/src/api/api.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UseCaseModule } from 'src/usecases/use-case.module';
 import { HealthController } from './health/health.controller';
 import { OrganizationController } from './organization/organization.controller';
+import { UserController } from './user/user.controller';
 
 @Module({
   imports: [UseCaseModule],
-  controllers: [HealthController, OrganizationController],
+  controllers: [HealthController, OrganizationController, UserController],
 })
 export class ApiModule {}
