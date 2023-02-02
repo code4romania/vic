ae473932e675d5cd1e516d69c090eec633a1633a
Oncescu Laurentiu <lari.oncescu@wearetribus.com>
2023-02-02 11:38:45 +0200

    fix:remove second ternary operator

--- frontend/src/components/FormInput.tsx
+++ frontend/src/components/FormInput.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import Input, { InputProps } from './Input';
+import { classNames } from '../common/utils/utils';
 
 interface FormInputProps extends InputProps {
   errorMessage?: string;
 }
 
-const FormInput = ({ errorMessage, readOnly, value, label, ...props }: FormInputProps) => {
+const FormInput = ({
+  errorMessage,
+  readOnly,
+  value,
+  label,
+  className,
+  helper,
+  ...props
+}: FormInputProps) => {
   return readOnly ? (
     <div className="flex gap-2.5 flex-col">
       <label>{label}</label>
       <p>{value}</p>
     </div>
-  ) : errorMessage ? (
+  ) : (
     <Input
       value={value}
       label={label}
-      className="border border-red-500 text-red-900 focus:ring-red-500 focus:border-red-500"
+      className={classNames(
+        errorMessage
+          ? 'border border-red-500 text-red-900 focus:ring-red-500 focus:border-red-500'
+          : '',
+        className,
+      )}
       {...props}
-      helper={<p className="text-red-500">{errorMessage}</p>}
+      helper={errorMessage ? <p className="text-red-500">{errorMessage}</p> : helper}
     />
-  ) : (
-    <Input value={value} label={label} {...props} />
   );
 };
 
