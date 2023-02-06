e9b9079948fb7f3c1df034c94362b00283ead60a
OlteanuCosmin <dj.cosmin99@gmail.com>
2023-02-06 16:05:59 +0200

    refactor(division.api.ts): remove test data

--- frontend/src/services/division/division.api.ts
+++ frontend/src/services/division/division.api.ts
@@ -1,7 +1,7 @@
 import { OrderDirection } from '../../common/enums/order-direction.enum';
 import { IPaginatedEntity } from '../../common/interfaces/paginated-entity.interface';
 import { DivisionType, IDivision } from '../../components/Divisions';
-// import API sfrom '../api';
+import API from '../api';
 
 export const getDivisionData = async (
   limit: number,
@@ -15,41 +15,6 @@ export const getDivisionData = async (
   if (orderBy) requestUrl = requestUrl.concat(`&orderBy=${orderBy}`);
 
   if (orderDirection) requestUrl = requestUrl.concat(`&orderDirection=${orderDirection}`);
-  console.log(requestUrl);
 
-  // return API.get(requestUrl).then((res) => res.data);
-
-  return {
-    items: [
-      {
-        id: '1',
-        name: 'Elena',
-        createdBy: {
-          id: '1',
-          name: 'Emag',
-        },
-        membersCount: 20,
-        createdOn: '10, 10, 2022',
-      },
-      {
-        id: '2',
-        name: 'Matei',
-        createdBy: {
-          id: '2',
-          name: 'Altex',
-        },
-        membersCount: 30,
-        createdOn: '11, 10, 2022',
-      },
-    ],
-    meta: {
-      currentPage: 1,
-      itemCount: 5,
-      itemsPerPage: 5,
-      totalItems: 10,
-      totalPages: 3,
-      orderByColumn: 'name',
-      orderDirection: OrderDirection.ASC,
-    },
-  };
+  return API.get(requestUrl).then((res) => res.data);
 };
