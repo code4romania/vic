8c62b25008307e622b4f90474bc0e74ff24fc8cc
Oncescu Laurentiu <lari.oncescu@wearetribus.com>
2023-02-06 12:50:47 +0200

    fix: remove organization profile from dashbord

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-// import { XMarkIcon } from '@heroicons/react/24/outline';
+import { XMarkIcon } from '@heroicons/react/24/outline';
 import { useTranslation } from 'react-i18next';
-// import CardBody from '../components/CardBody';
-// import CardHeader from '../components/CardHeader';
-// import Card from '../layouts/CardLayout';
+import CardBody from '../components/CardBody';
+import CardHeader from '../components/CardHeader';
+import Card from '../layouts/CardLayout';
 import PageLayout from '../layouts/PageLayout';
 import { useTestValuesQuery } from '../services/test/test.service';
-import OrganizationProfile from '../components/OrganizationProfile';
 
 const Dashboard = () => {
   // for testing purpose only
@@ -19,19 +18,7 @@ const Dashboard = () => {
       This is the Dashboard
       <h1>Translation test {t('test')}</h1>
       <p>{JSON.stringify(data)}</p>
-      <OrganizationProfile
-        organization={{
-          id: '1221312',
-          logo: 'sdasdasd',
-          email: 'contact@zen.ro',
-          name: 'Asociatia ZEN',
-          phone: '0721212121',
-          description:
-            'Descrierea organizației este afișată în profilul organizației din aplicația TEO. Poti alege sa formulezi o descriere specifica pentru aplicatia TEO, prin care sa atragi cat mai multi voluntari, sau sa folosesti descrierea organizatiei asa cum se regaseste in ONGHub. ',
-          address: 'Str Galbena, nr 129, ap 20, Iași, jud Iași',
-        }}
-      />
-      {/* <Card>
+      <Card>
         <CardHeader>
           <h4>Test header</h4>
           <XMarkIcon className="h-6 w-6" />
@@ -43,7 +30,7 @@ const Dashboard = () => {
             ex! Impedit omnis beatae, ducimus dolor at dolorem.
           </div>
         </CardBody>
-      </Card> */}
+      </Card>
     </PageLayout>
   );
 };
