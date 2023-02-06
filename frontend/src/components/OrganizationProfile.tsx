1af19b256a94b9f4666c3c05338c4e4b8364cf95
Oncescu Laurentiu <lari.oncescu@wearetribus.com>
2023-02-06 16:37:30 +0200

    fix: translation

--- frontend/src/components/OrganizationProfile.tsx
+++ frontend/src/components/OrganizationProfile.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import i18n from '../common/config/i18n';
 import Card from '../layouts/CardLayout';
 import Button from './Button';
 import CardHeader from './CardHeader';
@@ -6,7 +7,6 @@ import CardBody from './CardBody';
 import FormInput from './FormInput';
 import { PencilIcon } from '@heroicons/react/24/outline';
 import FormTextarea from './FormTextarea';
-import { useTranslation } from 'react-i18next';
 
 interface IOrganization {
   id: string;
@@ -23,15 +23,13 @@ interface OrganizationProfileProps {
 }
 
 const OrganizationProfile = ({ organization }: OrganizationProfileProps) => {
-  const { t } = useTranslation(['organization', 'general']);
-
   return (
     <Card>
       <CardHeader>
-        <h2>{t('title')}</h2>
+        <h2>{i18n.t('organization:title')}</h2>
         <Button
           className="btn-outline-secondary w-20"
-          label={t('edit')}
+          label={i18n.t('general:edit')}
           icon={<PencilIcon className="h-5 w-5 text-cool-gray-500" />}
           onClick={() => console.log('navigate')}
         />
@@ -39,32 +37,32 @@ const OrganizationProfile = ({ organization }: OrganizationProfileProps) => {
       <CardBody>
         <div className="flex flex-col gap-6 w-full lg:w-[80%] mx-auto pt-4 pb-16">
           <div className="flex flex-col gap-1">
-            <h2>{t('title')}</h2>
-            <p className="text-cool-gray-500">{t('subtitle')}</p>
+            <h2>{i18n.t('organization:title')}</h2>
+            <p className="text-cool-gray-500">{i18n.t('organization:subtitle')}</p>
           </div>
           <div className="flex flex-col gap-1">
-            <small className="text-cool-gray-500">{t('logo')}</small>
+            <small className="text-cool-gray-500">{i18n.t('organization:logo')}</small>
             <img
               src={organization.logo}
               alt="Organization Logo"
               className="h-28 object-contain self-start p-2"
             />
           </div>
-          <FormInput label={t('organization_naming')} value={organization.name} readOnly />
-          <FormInput label={t('email', { ns: 'general' })} value={organization.email} readOnly />
-          <FormInput label={t('phone', { ns: 'general' })} value={organization.phone} readOnly />
           <FormInput
-            label={t('address', { ns: 'general' })}
-            value={organization.address}
+            label={i18n.t('organization:organization_name')}
+            value={organization.name}
             readOnly
           />
+          <FormInput label={i18n.t('general:email')} value={organization.email} readOnly />
+          <FormInput label={i18n.t('general:phone')} value={organization.phone} readOnly />
+          <FormInput label={i18n.t('general:address')} value={organization.address} readOnly />
           <hr className="border-cool-gray-200" />
           <div className="flex flex-col gap-2">
-            <h2>{t('description_title')}</h2>
-            <p className="text-cool-gray-500">{t('description_placeholder')}</p>
+            <h2>{i18n.t('organization:description')}</h2>
+            <p className="text-cool-gray-500">{i18n.t('organization:description_placeholder')}</p>
           </div>
           <FormTextarea
-            label={t('description', { ns: 'general' })}
+            label={i18n.t('general:description')}
             defaultValue={organization.description}
             readOnly
           />
