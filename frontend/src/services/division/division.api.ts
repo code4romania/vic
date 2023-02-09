cfdaf2d1c0a9abc998920bec7dcb8409328b6419
OlteanuCosmin <dj.cosmin99@gmail.com>
2023-02-09 10:45:25 +0200

    refactor: (division.api.ts) remove test data

--- frontend/src/services/division/division.api.ts
+++ frontend/src/services/division/division.api.ts
@@ -10,53 +10,9 @@ export const getDivisions = async (
   orderBy?: string,
   orderDirection?: OrderDirection,
 ): Promise<IPaginatedEntity<IDivision>> => {
-  console.log(limit, page, divisionType, orderBy, orderDirection);
-  return {
-    items: [
-      {
-        id: '1',
-        name: 'Emag',
-        createdBy: {
-          id: '1',
-          name: 'Matei',
-        },
-        membersCount: 20,
-        createdOn: new Date(),
-      },
-      {
-        id: '2',
-        name: 'Altex',
-        createdBy: {
-          id: '1',
-          name: 'Matei',
-        },
-        membersCount: 20,
-        createdOn: new Date(),
-      },
-      {
-        id: '3',
-        name: 'Orange',
-        createdBy: {
-          id: '1',
-          name: 'Matei',
-        },
-        membersCount: 20,
-        createdOn: new Date(),
-      },
-    ],
-    meta: {
-      itemCount: 5,
-      totalItems: 10,
-      totalPages: 3,
-      itemsPerPage: 10,
-      currentPage: 1,
-      orderByColumn: 'name',
-      orderDirection: OrderDirection.ASC,
-    },
-  };
-  // return API.get(`/divisions`, {
-  //   params: { divisionType, limit, page, orderBy, orderDirection },
-  // }).then((res) => res.data);
+  return API.get(`/divisions`, {
+    params: { divisionType, limit, page, orderBy, orderDirection },
+  }).then((res) => res.data);
 };
 
 export const deleteDivision = async (id: string): Promise<void> => {
