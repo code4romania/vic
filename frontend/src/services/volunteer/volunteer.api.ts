6f75fa15624776788bcea8d5e99b5e1d0958cefe
OlteanuCosmin <dj.cosmin99@gmail.com>
2023-02-10 11:50:31 +0200

    chore: remove test data

--- frontend/src/services/volunteer/volunteer.api.ts
+++ frontend/src/services/volunteer/volunteer.api.ts
@@ -1,20 +1,6 @@
-import { Sex } from '../../common/enums/sex.enum';
 import { IAccessRequest } from '../../common/interfaces/access-request.interface';
-// import API from '../api';
+import API from '../api';
 
 export const getRegistration = async (id: string): Promise<IAccessRequest> => {
-  console.log(id);
-  return {
-    id: '1',
-    name: 'Elena MAtei',
-    logo: '',
-    age: 23,
-    sex: Sex.FEMALE,
-    location: 'Bucuresti',
-    email: 'hahaah',
-    phone: '1234567894',
-    createdOn: new Date(),
-    answers: ['raspuns 1', 'raspuns 2'],
-  };
-  // return API.get(`/volunteers/access-request/${id}`).then((res) => res.data);
+  return API.get(`/volunteers/access-request/${id}`).then((res) => res.data);
 };
