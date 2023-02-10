ae98089d78590dceccdd90ee77e47ac53b6f138c
Oncescu Laurentiu <lari.oncescu@wearetribus.com>
2023-02-10 13:58:33 +0200

    refactor(registration-requests): sort data in table

--- frontend/src/pages/RegistrationRequests.tsx
+++ frontend/src/pages/RegistrationRequests.tsx
@@ -9,8 +9,11 @@ import DataTableComponent from '../components/DataTableComponent';
 import i18n from '../common/config/i18n';
 import { ArrowDownTrayIcon, CheckIcon, EyeIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import { useRegistrationRequestsQuery } from '../services/registration-requests/registration-requests.service';
-import { TableColumn } from 'react-data-table-component';
+import { SortOrder, TableColumn } from 'react-data-table-component';
 import Popover from '../components/Popover';
+import { OrderDirection } from '../common/enums/order-direction.enum';
+import { SelectItem } from '../components/Select';
+import { PaginationConfig } from '../common/constants/pagination';
 
 export interface IAccessRequest {
   id: string;
@@ -21,6 +24,16 @@ export interface IAccessRequest {
   phone: string;
   createdOn: string;
 }
+
+export enum FilterType {
+  Pending = 'pending',
+  Rejected = 'rejected',
+}
+
+const RegistrationRequestsTabs: SelectItem<FilterType>[] = [
+  { key: FilterType.Pending, value: i18n.t('registration_requests:options.requests') },
+  { key: FilterType.Rejected, value: i18n.t('registration_requests:options.rejected_requests') },
+];
 
 const RegistrationRequestsTableHeader = [
   {
@@ -54,56 +67,65 @@ const RegistrationRequestsTableHeader = [
   },
 ];
 
-// row actions
-const onView = (row: IAccessRequest) => {
-  alert(`Not yet implemented, ${row}`);
-};
-
-const onApprove = (row: IAccessRequest) => {
-  console.log(`Not yet implemented, ${row}`);
-};
-
-const onReject = (row: IAccessRequest) => {
-  alert(`Not yet implemented, ${row}`);
-};
-
-// menu items
-const buildRegistrationRequestsActionColumn = (): TableColumn<IAccessRequest> => {
-  const registrationRequestsMenuItems = [
-    {
-      label: i18n.t('registration_requests:modal.view'),
-      icon: <EyeIcon className="menu-icon" />,
-      onClick: onView,
-    },
-    {
-      label: i18n.t('registration_requests:modal.approve'),
-      icon: <CheckIcon className="menu-icon" />,
-      onClick: onApprove,
-    },
-    {
-      label: i18n.t('registration_requests:modal.reject'),
-      icon: <XMarkIcon className="menu-icon" />,
-      onClick: onReject,
-      alert: true,
-    },
-  ];
-
-  return {
-    name: '',
-    cell: (row: IAccessRequest) => (
-      <Popover<IAccessRequest> row={row} items={registrationRequestsMenuItems} />
-    ),
-    width: '50px',
-    allowOverflow: true,
-  };
-};
-
 const RegistrationRequests = () => {
-  const [filterStatus, setFilterStatus] = useState<'pending' | 'rejected'>('pending');
+  const [filterStatus, setFilterStatus] = useState<FilterType>(FilterType.Pending);
   const [page, setPage] = useState<number>();
   const [rowsPerPage, setRowsPerPage] = useState<number>();
   const [orderByColumn, setOrderByColumn] = useState<string>();
   const [orderDirection, setOrderDirection] = useState<OrderDirection>();
+
+  // row actions
+  const onView = (row: IAccessRequest) => {
+    alert(`Not yet implemented, ${row}`);
+  };
+
+  const onApprove = (row: IAccessRequest) => {
+    console.log(`Not yet implemented, ${row}`);
+  };
+
+  const onReject = (row: IAccessRequest) => {
+    alert(`Not yet implemented, ${row}`);
+  };
+
+  // menu items
+  const buildRegistrationRequestsActionColumn = (): TableColumn<IAccessRequest> => {
+    const registrationRequestsMenuItems = [
+      {
+        label: i18n.t('registration_requests:modal.view'),
+        icon: <EyeIcon className="menu-icon" />,
+        onClick: onView,
+      },
+      {
+        label: i18n.t('registration_requests:modal.approve'),
+        icon: <CheckIcon className="menu-icon" />,
+        onClick: onApprove,
+      },
+      {
+        label: i18n.t('registration_requests:modal.reject'),
+        icon: <XMarkIcon className="menu-icon" />,
+        onClick: onReject,
+        alert: true,
+      },
+    ];
+
+    return {
+      name: '',
+      cell: (row: IAccessRequest) => (
+        <Popover<IAccessRequest> row={row} items={registrationRequestsMenuItems} />
+      ),
+      width: '50px',
+      allowOverflow: true,
+    };
+  };
+
+  const onSort = (column: TableColumn<IAccessRequest>, direction: SortOrder) => {
+    setOrderByColumn(column.id as string);
+    setOrderDirection(
+      direction.toLocaleUpperCase() === OrderDirection.ASC
+        ? OrderDirection.ASC
+        : OrderDirection.DESC,
+    );
+  };
 
   const { data: registrationRequests, isLoading } = useRegistrationRequestsQuery(
     filterStatus,
@@ -122,20 +144,14 @@ const RegistrationRequests = () => {
     }
   }, []);
 
-  const handleTabClick = (key: number) => {
-    console.log(key);
+  const handleTabClick = (key: FilterType) => {
+    setFilterStatus(key);
   };
 
   return (
     <PageLayout>
       <h1>{i18n.t('registration_requests:title')}</h1>
-      <Tabs
-        tabs={[
-          { value: i18n.t('registration_requests:options.requests'), key: 2 },
-          { value: i18n.t('registration_requests:options.rejected_requests'), key: 3 },
-        ]}
-        onClick={handleTabClick}
-      >
+      <Tabs<FilterType> tabs={RegistrationRequestsTabs} onClick={handleTabClick}>
         <Card>
           <CardHeader>
             <div></div>
@@ -155,9 +171,13 @@ const RegistrationRequests = () => {
               data={registrationRequests?.items}
               loading={isLoading}
               pagination
-              onChangePage={setPage}
+              paginationPerPage={rowsPerPage}
+              paginationRowsPerPageOptions={PaginationConfig.rowsPerPageOptions}
+              paginationTotalRows={registrationRequests?.meta?.totalItems}
+              paginationDefaultPage={page}
               onChangeRowsPerPage={setRowsPerPage}
-              page={page}
+              onChangePage={setPage}
+              onSort={onSort}
             />
           </CardBody>
         </Card>
--- frontend/src/services/registration-requests/registration-requests.api.ts
+++ frontend/src/services/registration-requests/registration-requests.api.ts
@@ -1,6 +1,6 @@
 import { IAccessRequest } from '../../pages/RegistrationRequests';
 import { IPaginatedEntity } from '../../common/interfaces/paginated-entity.interface';
-import API from '../api';
+// import API from '../api';
 import { OrderDirection } from '../../common/enums/order-direction.enum';
 
 export const getRegistrationRequests = async (
@@ -10,84 +10,84 @@ export const getRegistrationRequests = async (
   orderBy?: string,
   orderDirection?: OrderDirection,
 ): Promise<IPaginatedEntity<IAccessRequest>> => {
-  return API.get('/volunteers/registration-requests', {
-    params: { limit, page, filterStatus, orderBy, orderDirection },
-  }).then((res) => res.data);
-  // console.log(status);
-  // return Promise.resolve({
-  //   items: [
-  //     {
-  //       id: '1',
-  //       logo: 'https://company1.com/logo.png',
-  //       name: 'Company 1',
-  //       address: 'San Francisco, CA, USA',
-  //       email: 'info@company1.com',
-  //       phone: '+1 123 456 7890',
-  //       createdOn: '2022-01-01T12:00:00.000Z',
-  //     },
-  //     {
-  //       id: '2',
-  //       logo: 'https://company2.com/logo.png',
-  //       name: 'Company 2',
-  //       address: 'New York, NY, USA',
-  //       email: 'info@company2.com',
-  //       phone: '+1 234 567 8901',
-  //       createdOn: '2022-02-01T12:00:00.000Z',
-  //     },
-  //     {
-  //       id: '3',
-  //       logo: 'https://company3.com/logo.png',
-  //       name: 'Company 3',
-  //       address: 'London, UK',
-  //       email: 'info@company3.com',
-  //       phone: '+44 123 456 7890',
-  //       createdOn: '2022-03-01T12:00:00.000Z',
-  //     },
-  //     {
-  //       id: '4',
-  //       logo: 'https://company4.com/logo.png',
-  //       name: 'Company 4',
-  //       address: 'Berlin, Germany',
-  //       email: 'info@company4.com',
-  //       phone: '+49 123 456 7890',
-  //       createdOn: '2022-04-01T12:00:00.000Z',
-  //     },
-  //     {
-  //       id: '5',
-  //       logo: 'https://company5.com/logo.png',
-  //       name: 'Company 5',
-  //       address: 'Paris, France',
-  //       email: 'info@company5.com',
-  //       phone: '+33 123 456 7890',
-  //       createdOn: '2022-05-01T12:00:00.000Z',
-  //     },
-  //     {
-  //       id: '6',
-  //       logo: 'https://company6.com/logo.png',
-  //       name: 'Company 6',
-  //       address: 'Sydney, Australia',
-  //       email: 'info@company6.com',
-  //       phone: '+61 123 456 7890',
-  //       createdOn: '2022-06-01T12:00:00.000Z',
-  //     },
-  //     {
-  //       id: '7',
-  //       logo: 'https://company7.com/logo.png',
-  //       name: 'Company 7',
-  //       address: 'Tokyo, Japan',
-  //       email: 'info@company7.com',
-  //       phone: '+81 123 456 7890',
-  //       createdOn: '2022-07-01T12:00:00.000Z',
-  //     },
-  //   ],
-  //   meta: {
-  //     currentPage: 3,
-  //     itemCount: 3,
-  //     itemsPerPage: 3,
-  //     totalItems: 3,
-  //     totalPages: 3,
-  //     orderByColumn: 'name',
-  //     orderDirection: 'ASC',
-  //   },
-  // });
+  // return API.get('/volunteers/registration-requests', {
+  //   params: { limit, page, filterStatus, orderBy, orderDirection },
+  // }).then((res) => res.data);
+  return Promise.resolve({
+    items: [
+      {
+        id: '1',
+        logo: '/logo.svg',
+        name: 'Company 1',
+        address: 'San Francisco, CA, USA',
+        email: 'info@company1.com',
+        phone: '+1 123 456 7890',
+        createdOn: '2022-01-01T12:00:00.000Z',
+      },
+      {
+        id: '2',
+        logo: '/logo.svg',
+        name: 'Company 2',
+        address: 'New York, NY, USA',
+        email: 'info@company2.com',
+        phone: '+1 234 567 8901',
+        createdOn: '2022-02-01T12:00:00.000Z',
+      },
+      {
+        id: '3',
+        logo: '/logo.svg',
+        name: 'Company 3',
+        address: 'London, UK',
+        email: 'info@company3.com',
+        phone: '+44 123 456 7890',
+        createdOn: '2022-03-01T12:00:00.000Z',
+      },
+      {
+        id: '4',
+        logo: '/logo.svg',
+        name: 'Company 4',
+        address: 'Berlin, Germany',
+        email: 'info@company4.com',
+        phone: '+49 123 456 7890',
+        createdOn: '2022-04-01T12:00:00.000Z',
+      },
+      {
+        id: '5',
+        logo: '/logo.svg',
+        name: 'Company 5',
+        address: 'Paris, France',
+        email: 'info@company5.com',
+        phone: '+33 123 456 7890',
+        createdOn: '2022-05-01T12:00:00.000Z',
+      },
+      {
+        id: '6',
+        logo: '/logo.svg',
+        name: 'Company 6',
+        address: 'Sydney, Australia',
+        email: 'info@company6.com',
+        phone: '+61 123 456 7890',
+        createdOn: '2022-06-01T12:00:00.000Z',
+      },
+      {
+        id: '7',
+        logo: '/logo.svg',
+        name: 'Company 7',
+        address: 'Tokyo, Japan',
+        email: 'info@company7.com',
+        phone: '+81 123 456 7890',
+        createdOn: '2022-07-01T12:00:00.000Z',
+      },
+    ],
+    meta: {
+      status: filterStatus,
+      currentPage: page,
+      itemCount: 3,
+      itemsPerPage: limit,
+      totalItems: 7,
+      totalPages: 2,
+      orderByColumn: orderBy || 'name',
+      orderDirection: orderDirection || OrderDirection.ASC,
+    },
+  });
 };
