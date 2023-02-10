4efe8d4b13fb345fb01957f3003de773cdf1077c
OlteanuCosmin <dj.cosmin99@gmail.com>
2023-02-10 12:02:47 +0200

    refactor: (auth api) revert changes

--- frontend/src/services/auth/auth.api.ts
+++ frontend/src/services/auth/auth.api.ts
@@ -1,7 +1,6 @@
 import { IUser } from '../../common/interfaces/user.interface';
-// import API from '../api';
+import API from '../api';
 
 export const login = async (): Promise<IUser> => {
-  // return API.post('auth/login').then((res) => res.data);
-  return Promise.resolve({ id: 'HahaProduction', name: 'Andrei Milioane' });
+  return API.post('auth/login').then((res) => res.data);
 };
