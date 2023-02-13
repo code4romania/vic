ae2a7a5d360e046bf0a505867ff7e75e99244314
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2023-02-13 12:18:53 +0200

    fix: vite build config fixes

--- frontend/src/common/interfaces/route.interface.ts
+++ frontend/src/common/interfaces/route.interface.ts
@@ -2,5 +2,10 @@ export interface IRoute {
   id: number;
   name: string;
   href: string;
-  icon: React.FunctionComponent<{ className: string }>;
+  icon: React.ForwardRefExoticComponent<
+    React.SVGProps<SVGSVGElement> & {
+      title?: string | undefined;
+      titleId?: string | undefined;
+    }
+  >;
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import polyfillNode from 'rollup-plugin-polyfill-node';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    global: {},
+  build: {
+    rollupOptions: {
+      plugins: [polyfillNode()],
+    },
+  },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
   },
   plugins: [
     react(),
