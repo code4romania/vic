47a4317105729c9c41da95e1757afaef2d04e66a
Andrei Ioniță <hi@andrei.io>
2023-02-14 14:06:39 +0000

    chore(infrastructure): fix env vars

--- terraform/amplify.tf
+++ terraform/amplify.tf
@@ -19,8 +19,8 @@ resource "aws_amplify_app" "amplify_app" {
   }
 }
 
-  # branch_name = var.env == "production" ? "main" : "develop"
-  # stage       = var.env == "production" ? "PRODUCTION" : "BETA"
+# branch_name = var.env == "production" ? "main" : "develop"
+# stage       = var.env == "production" ? "PRODUCTION" : "BETA"
 resource "aws_amplify_branch" "branch" {
   app_id      = aws_amplify_app.amplify_app.id
   branch_name = "main"
@@ -30,17 +30,17 @@ resource "aws_amplify_branch" "branch" {
   enable_auto_build = true
 
   environment_variables = {
-    AMPLIFY_DIFF_DEPLOY            = false
-    AMPLIFY_MONOREPO_APP_ROOT      = "frontend"
-    
-    VITE_API_URL                   = "https://${aws_apprunner_service.backend.service_url}"
-    VITE_APP_FRONTEND_URL          = "https://${local.frontend_domain}/"
+    AMPLIFY_DIFF_DEPLOY       = false
+    AMPLIFY_MONOREPO_APP_ROOT = "frontend"
 
-    # ONGHub User Pool for TEO Client 
-    VITE_AWS_REGION                = "eu-west-1"
-    VITE_COGNITO_OAUTH_DOMAIN      = "onghub-staging.auth.eu-west-1.amazoncognito.com"
-    VITE_USER_POOL_CLIENT_ID       = "51i4lqdq87kadd5iqqfle23vld"
-    VITE_USER_POOL_ID              = "eu-west-1_HlcsM9cmP"
+    VITE_API_URL          = "https://${aws_apprunner_service.backend.service_url}"
+    VITE_APP_FRONTEND_URL = "https://${local.frontend_domain}"
+
+    # ONGHub User Pool for TEO Client
+    VITE_AWS_REGION           = var.onghub_congito_region
+    VITE_COGNITO_OAUTH_DOMAIN = var.onghub_cognito_oauth_domain
+    VITE_USER_POOL_CLIENT_ID  = var.onghub_cognito_teo_client_id
+    VITE_USER_POOL_ID         = var.onghub_cognito_user_pool_id
   }
 }
 
