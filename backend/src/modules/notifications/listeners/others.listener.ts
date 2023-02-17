6f18f429a0b2054f98900488bbe8335beb29c0b4
OlteanuCosmin <dj.cosmin99@gmail.com>
2023-02-17 12:13:28 +0200

    refactor(accouncement-event): fix typo, change announcement event params

--- backend/src/modules/notifications/listeners/others.listener.ts
+++ backend/src/modules/notifications/listeners/others.listener.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { EVENTS } from '../constants/events.constants';
-import SendAccouncementEvent from '../events/others/send-accouncement.event';
+import SendAnnouncementEvent from '../events/others/send-announcement.event';
 
 @Injectable()
 export class OthersListener {
   @OnEvent(EVENTS.OTHER.SEND_ANNOUNCEMENT)
-  async sendSendAccouncementEvent(
-    payload: SendAccouncementEvent,
+  async sendSendAnnouncementEvent(
+    payload: SendAnnouncementEvent,
   ): Promise<void> {
     console.log(payload);
-    // TODO: 1. Retrieve the user data
-    // TODO: 2. Throw exception if user not found
-    // TODO: 3. Retrive the accouncement data
-    // TODO: 4. Throw exception if accouncement not found
-    // TODO: 5. Send push notification to user with annoucement link
+    // TODO: 1. Retrieve the organization data
+    // TODO: 2. Throw exception if organization not found
+    // TODO: 3. Retrive the announcement data
+    // TODO: 4. Throw exception if announcement not found
+    // TODO: 5. Retrieve the target data
+    // TODO: 6. Throw exception if target not found
+    // TODO: 7. Send push notification to target with announcement link
   }
 }
