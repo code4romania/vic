4ed192f758a599e6c6917ef9c27478d23c6c8248
Oncescu Laurentiu <lari.oncescu@wearetribus.com>
2023-02-20 16:59:43 +0200

    refcator: add error and mock api

--- frontend/src/common/errors/internal-errors.class.ts
+++ frontend/src/common/errors/internal-errors.class.ts
@@ -1,4 +1,5 @@
 import { AccessCodeError } from './entities/access-request.errors';
+import { ActivityCategoryError } from './entities/activty-category.errors';
 import { DivisionError } from './entities/division.errors';
 import { OrganizationError } from './entities/organization.errors';
 import { VolunteerError } from './entities/volunteer.errors';
@@ -9,4 +10,5 @@ export class InternalErrors {
   public static DIVISION_ERRORS = DivisionError.getInstance();
   public static VOLUNTEER_ERRORS = VolunteerError.getInstance();
   public static ACCESS_CODE_ERRORS = AccessCodeError.getInstance();
+  public static ACTIVITY_CATEGORY_ERRORS = ActivityCategoryError.getInstance();
 }
--- frontend/src/pages/ActivityCategories.tsx
+++ frontend/src/pages/ActivityCategories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import i18n from '../common/config/i18n';
 import Button from '../components/Button';
 import PageLayout from '../layouts/PageLayout';
@@ -7,9 +7,44 @@ import { useNavigate } from 'react-router';
 import Card from '../layouts/CardLayout';
 import CardHeader from '../components/CardHeader';
 import CardBody from '../components/CardBody';
+import { useActivityCategoriesQuery } from '../services/activity-category/activity-categories.service';
+import EmptyContent from '../components/EmptyContent';
+import LoadingContent from '../components/LoadingContent';
+import { useErrorToast } from '../hooks/useToast';
+import { InternalErrors } from '../common/errors/internal-errors.class';
+
+export enum CategoryStatus {
+  ACTIVE = 'active',
+  DISABLED = 'disabled',
+}
+
+export interface IActivityCategory {
+  id: string;
+  name: string;
+  icon: string;
+  role: string;
+  department: string;
+  branch: string;
+  status: CategoryStatus;
+}
 
 const ActivityCategories = () => {
   const navigate = useNavigate();
+
+  const {
+    data: activityCategories,
+    error: activityCategoriesError,
+    isLoading: isActivityCategoriesLoading,
+  } = useActivityCategoriesQuery();
+
+  useEffect(() => {
+    if (activityCategoriesError)
+      useErrorToast(
+        InternalErrors.ACTIVITY_CATEGORY_ERRORS.getError(
+          activityCategoriesError.response?.data.code_error,
+        ),
+      );
+  }, [activityCategoriesError]);
 
   const onAdd = () => {
     navigate('add');
@@ -26,14 +61,24 @@ const ActivityCategories = () => {
           onClick={onAdd}
         />
       </div>
-      <Card>
-        <CardHeader>
-          <h2>{i18n.t('side_menu:options.activity_categories')}</h2>
-        </CardHeader>
-        <CardBody>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3"></div>
-        </CardBody>
-      </Card>
+      {isActivityCategoriesLoading && <LoadingContent />}
+      {activityCategories && !isActivityCategoriesLoading && (
+        <Card>
+          <CardHeader>
+            <h2>{i18n.t('side_menu:options.activity_categories')}</h2>
+          </CardHeader>
+          <CardBody>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+              {activityCategories.map((activity: IActivityCategory) => (
+                <div key={activity.id}></div>
+              ))}
+            </div>
+          </CardBody>
+        </Card>
+      )}
+      {!activityCategories && !isActivityCategoriesLoading && (
+        <EmptyContent description={i18n.t('general:error.load_entries')} />
+      )}
     </PageLayout>
   );
 };
