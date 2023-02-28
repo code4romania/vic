d1380724f98298b79c96a8e3df14f0d5f51be63d
Oncescu Laurentiu <lari.oncescu@wearetribus.com>
2023-02-28 10:57:50 +0200

    fix: improper display of alt attribute on img tag

--- frontend/src/components/MediaCell.tsx
+++ frontend/src/components/MediaCell.tsx
@@ -8,7 +8,7 @@ interface MediaCellProps {
 
 const MediaCell = ({ logo, title, subtitle }: MediaCellProps) => (
   <div className="flex items-center gap-3 shrink-0 w-full">
-    <img src={logo} className="w-10 h-10 logo" alt={`${title} picture`} />
+    <img src={logo} className="w-10 h-10 logo text-transparent" alt={`${title} picture`} />
     <div className="flex flex-col w-4/5">
       <small className="font-robotoBold truncate ">{title}</small>
       <small className="truncate text-cool-gray-500">{subtitle || ''}</small>
