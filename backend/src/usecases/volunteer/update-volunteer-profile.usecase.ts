275a6578990c712add43e5860db0d5df281cc94a
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2023-03-02 11:13:45 +0200

    refactor(volunteers): map api errors

--- backend/src/usecases/volunteer/update-volunteer-profile.usecase.ts
+++ backend/src/usecases/volunteer/update-volunteer-profile.usecase.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { IUseCaseService } from 'src/common/interfaces/use-case-service.interface';
 import { ExceptionsService } from 'src/infrastructure/exceptions/exceptions.service';
-import { VolunteerStatus } from 'src/modules/volunteer/enums/volunteer-status.enum';
 import { VolunteerExceptionMessages } from 'src/modules/volunteer/exceptions/volunteer.exceptions';
 import { UpdateVolunteerProfileOptions } from 'src/modules/volunteer/model/volunteer-profile.model';
 import { IVolunteerModel } from 'src/modules/volunteer/model/volunteer.model';
