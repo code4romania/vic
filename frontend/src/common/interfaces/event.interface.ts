f8917d4e57e9f9bdbc167cec0f7a8072f4e425e4
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2023-03-10 15:34:11 +0200

    refactor(event): update event interface with correct types

--- frontend/src/common/interfaces/event.interface.ts
+++ frontend/src/common/interfaces/event.interface.ts
@@ -1,6 +1,6 @@
 import { AttendanceType } from '../enums/attendance-type.enum';
-import { DivisionType } from '../enums/division-type.enum';
 import { EventStatus } from '../enums/event-status';
+import { IDivisionListItem } from './division.interface';
 
 export interface IEvent {
   id: string;
@@ -14,9 +14,8 @@ export interface IEvent {
   observation?: string;
   attendanceType: AttendanceType;
   attendanceMention: string;
-  tasks: Array<{ id: string; name: string }>;
-  targets: Array<{ id: string; name: string; type: DivisionType; members: number }>;
+  tasks: IDivisionListItem[];
+  targets: IDivisionListItem[];
   rsvp: { yes: number; no: number };
   status: EventStatus;
-  reportedHours: string;
 }
