dce07faf02d3da21339dd9e1ab002571fab61ae4
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2023-03-15 13:29:04 +0200

    refactor(edit-event): correctly map event form data, and format api payload

--- frontend/src/services/event/event.api.ts
+++ frontend/src/services/event/event.api.ts
@@ -1,4 +1,5 @@
 import { EventState } from '../../common/enums/event-state.enum';
+import { EventStatus } from '../../common/enums/event-status';
 import { OrderDirection } from '../../common/enums/order-direction.enum';
 import { IEvent } from '../../common/interfaces/event.interface';
 import { IPaginatedEntity } from '../../common/interfaces/paginated-entity.interface';
@@ -6,14 +7,14 @@ import { EventFormTypes, TargetType } from '../../components/EventForm';
 import API from '../api';
 
 export const addEvent = async (data: EventFormTypes): Promise<IEvent> => {
-  return API.post('event', { ...formatEventPayload(data) }).then((res) => res.data);
+  return API.post('event', { ...formatAddEventPayload(data) }).then((res) => res.data);
 };
 
 export const editEvent = async (
   id: string,
   data: Omit<EventFormTypes, 'status'>,
 ): Promise<IEvent> => {
-  return API.patch(`event/${id}`, { ...formatEventPayload(data) }).then((res) => res.data);
+  return API.patch(`event/${id}`, { ...formatEditEventPayload(data) }).then((res) => res.data);
 };
 
 export const getEvent = async (id: string): Promise<IEvent> => {
@@ -38,7 +39,7 @@ export const getEvents = async (
   }).then((res) => res.data);
 };
 
-const formatEventPayload = (data: EventFormTypes): object => {
+const formatAddEventPayload = (data: EventFormTypes): object => {
   const { targets, tasks, targetType, ...payload } = data;
   return {
     ...payload,
@@ -46,6 +47,22 @@ const formatEventPayload = (data: EventFormTypes): object => {
     isPublic: targetType === TargetType.PUBLIC,
     ...(targetType === TargetType.SELECT
       ? { targetsIds: targets.map((target) => target.key) }
+      : { targetsIds: [] }),
+  };
+};
+
+const formatEditEventPayload = (data: EventFormTypes): object => {
+  const { targets, tasks, targetType, status, ...payload } = data;
+
+  return {
+    ...payload,
+    tasksIds: tasks.map((task) => task.key),
+    ...(status === EventStatus.DRAFT
+      ? {
+          ...(targetType === TargetType.SELECT
+            ? { targetsIds: targets.map((target) => target.key) }
+            : { targetsIds: [] }),
+        }
       : {}),
   };
 };
