ee7fd5de30bdb46a0887d83509e56487fa5b0d74
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2023-03-31 16:06:08 +0300

    refactor(dashboard): update dashboard layout

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -10,16 +10,18 @@ const Dashboard = () => {
   return (
     <PageLayout>
       <PageHeader>Dashboard</PageHeader>
-      <div className="flex gap-6 xl:gap-12 flex-col md:flex-row">
-        <div className="flex gap-6 flex-wrap md:self-start">
-          <ActivityLogStatisticsCard />
-          <VolunteerStatisticsCard />
+      <div className="flex flex-col gap-y-6 md:gap-8">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-y-6 md:gap-y-8 lg:gap-8">
+          <div className="flex flex-col sm:flex-row lg:flex-col gap-6 md:self-start">
+            <ActivityLogStatisticsCard />
+            <VolunteerStatisticsCard />
+          </div>
+          <div className="grow w-full lg:col-span-2 ">
+            <PieChartCard />
+          </div>
         </div>
-        <div className="grow w-full">
-          <PieChartCard />
-        </div>
+        <LineChartCard />
       </div>
-      <LineChartCard />
     </PageLayout>
   );
 };
