02c8f7e7383c937b313e4a6efa0e9bee6e635ea7
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2023-03-31 18:22:03 +0300

    chore: add other scenario

--- frontend/src/components/ActivityLogTable.tsx
+++ frontend/src/components/ActivityLogTable.tsx
@@ -47,7 +47,7 @@ const PendingActivityLogTableHeader = [
     cell: (row: IActivityLogListItem) => (
       <MediaCell
         logo={row.activityType?.icon}
-        title={row.activityType.name}
+        title={row.activityType?.name || i18n.t('general:other')}
         subtitle={row.event?.name || ''}
       />
     ),
@@ -88,7 +88,7 @@ const PastActivityLogTableHeader = [
     cell: (row: IActivityLogListItem) => (
       <MediaCell
         logo={row.activityType?.icon}
-        title={row.activityType.name}
+        title={row.activityType?.name || i18n.t('general:other')}
         subtitle={row.event?.name || ''}
       />
     ),
