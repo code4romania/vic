45eedb8b63b1f9c8d75a4ac8ecfc8ea540de406b
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2023-04-05 14:46:48 +0300

    refactor(actions-archive): simplify LinkText compoent

--- frontend/src/components/LinkText.tsx
+++ frontend/src/components/LinkText.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import { useNavigate } from 'react-router';
 
 interface ILinkTextProps {
   url: string;
   content: React.ReactNode;
-  title?: string;
 }
 
-export const LinkText = ({ url, title, content }: ILinkTextProps) => {
-  const navigate = useNavigate();
-
-  const onLinkClick = () => {
-    navigate(url, { replace: true });
-  };
-
-  return (
-    <span
-      onClick={onLinkClick}
-      title={title || ''}
-      className="text-turquoise hover:text-turquoise-700 active:text-turquoise-800 cursor-pointer text-xs sm:text-sm font-normal"
-    >
-      {content}
-    </span>
-  );
-};
+export const LinkText = ({ url, content }: ILinkTextProps) => (
+  <a href={url} target="_blank" className="text-xs sm:text-sm" rel="noreferrer">
+    {content}
+  </a>
+);
