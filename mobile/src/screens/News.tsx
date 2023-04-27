db5ac6e71d69a352e8fec2c60ba3a5b0f8359924
Oncescu Laurentiu <lari.oncescu@wearetribus.com>
2023-04-27 09:56:36 +0300

    feat: add news list

--- mobile/src/screens/News.tsx
+++ mobile/src/screens/News.tsx
@@ -1,15 +1,55 @@
 import React from 'react';
 import PageLayout from '../layouts/PageLayout';
-import { Text } from '@ui-kitten/components';
+import { VirtualizedList, View, StyleSheet } from 'react-native';
+import NewsListItem from '../components/NewsListItem';
+import { SvgXml } from 'react-native-svg';
+import LogoSvg from '../assets/svg/logo.js';
+
+const Separator = () => <View style={styles.separator} />;
 
 const News = ({ navigation }: any) => {
   console.log('News');
 
+  const getItem = (data, index) => {
+    return {
+      id: Math.random().toString(12).substring(0),
+      title: `Item ${index + 1}`,
+    };
+  };
+
+  const getItemCount = (data: unknown) => {
+    return 50;
+  };
+
+  const renderItem = ({ item }) => (
+    <NewsListItem
+      icon={<SvgXml xml={LogoSvg} width={24} height={24} />}
+      title="Important! Ne vedem maine la 10!"
+      subtitle="La 10:30 este plecarea, nu intarziati!"
+    />
+  );
+
   return (
     <PageLayout title="News" onBackButtonPress={navigation.goBack}>
-      <Text category="h1">News</Text>
+      <VirtualizedList
+        getItem={getItem}
+        getItemCount={getItemCount}
+        renderItem={renderItem}
+        ItemSeparatorComponent={Separator}
+        style={styles.virtualizedList}
+      />
     </PageLayout>
   );
 };
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 16,
+  },
+  virtualizedList: {
+    width: '100%',
+    paddingHorizontal: 16,
+  },
+});
 
 export default News;
