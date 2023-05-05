0b14854a76c8b86593921c99b83afd00a5418757
Oncescu Laurentiu <lari.oncescu@wearetribus.com>
2023-05-05 17:38:40 +0300

    feat: add organizations list

--- mobile/src/screens/Organizations.tsx
+++ mobile/src/screens/Organizations.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import PageLayout from '../layouts/PageLayout';
-import { Button, Text } from '@ui-kitten/components';
+import { Text, List, Avatar, Divider } from '@ui-kitten/components';
+import { Pressable, StyleSheet, View } from 'react-native';
+
+const organizations = [
+  { logo: 'https://picsum.photos/200', name: 'Asociatia ZEN', volunteers: 1200 },
+  { logo: 'https://picsum.photos/200', name: 'Green City', volunteers: 60 },
+  { logo: 'https://picsum.photos/200', name: 'Code4Romania', volunteers: 2003 },
+  { logo: 'https://picsum.photos/200', name: 'UNICEF', volunteers: 20 },
+  { logo: 'https://picsum.photos/200', name: 'EESTEC Bucuresti', volunteers: 121 },
+  { logo: 'https://picsum.photos/200', name: 'Habitat for Humanity', volunteers: 12211212 },
+];
 
 const Organizations = ({ navigation }: any) => {
   console.log('Organizations');
@@ -9,12 +19,56 @@ const Organizations = ({ navigation }: any) => {
     navigation.navigate('organization-profile');
   };
 
+  const renderItem = ({ item }: { item: { logo: string; name: string; volunteers: number } }) => {
+    return (
+      <Pressable
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed ? '#d9d9d9' : '#fff',
+          },
+        ]}
+        onPress={onViewOrganizationProfileButtonPress}
+      >
+        <View style={styles.renderItem}>
+          <Avatar source={{ uri: item.logo }} size="large" />
+          <View style={styles.textWrapper}>
+            <Text category="label">{item.name}</Text>
+            <Text category="c1" appearance="hint">
+              {item.name}
+            </Text>
+          </View>
+        </View>
+      </Pressable>
+    );
+  };
+
   return (
     <PageLayout title="Organizations">
-      <Text category="h1">Organizations</Text>
-      <Button onPress={onViewOrganizationProfileButtonPress}>View Organization Profile</Button>
+      <List
+        data={organizations}
+        renderItem={renderItem}
+        style={styles.list}
+        ItemSeparatorComponent={Divider}
+      />
     </PageLayout>
   );
 };
 
 export default Organizations;
+
+const styles = StyleSheet.create({
+  list: {
+    backgroundColor: '#fff',
+  },
+  textWrapper: {
+    gap: 4,
+  },
+  renderItem: {
+    gap: 16,
+    paddingVertical: 16,
+    paddingHorizontal: 0,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
