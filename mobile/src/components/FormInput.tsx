e8dcf296e8a8dd6b9017a6aad351486698a5edd7
Oncescu Laurentiu <lari.oncescu@wearetribus.com>
2023-05-09 17:39:53 +0300

    refactor: add styles on error, remove paragraph and change theme

--- mobile/src/components/FormInput.tsx
+++ mobile/src/components/FormInput.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Input, InputProps, Text } from '@ui-kitten/components';
+import { Input, InputProps, StyleService, Text, useStyleSheet } from '@ui-kitten/components';
 import { Control, Controller } from 'react-hook-form';
-import { View, StyleSheet, Keyboard } from 'react-native';
+import { View, Keyboard } from 'react-native';
 
 interface FormInputProps extends InputProps {
   control: Control<Record<string, any>>;
@@ -10,6 +10,7 @@ interface FormInputProps extends InputProps {
   placeholder: string;
   secureTextEntry?: boolean;
   error: any;
+  required?: boolean;
 }
 
 const FormInput: React.FC<FormInputProps> = ({
@@ -19,15 +20,21 @@ const FormInput: React.FC<FormInputProps> = ({
   placeholder,
   secureTextEntry = false,
   error,
+  required,
   ...rest
 }: FormInputProps) => {
+  const styles = useStyleSheet(themedStyles);
+
   const handleKeyboardDismiss = () => {
     Keyboard.dismiss();
   };
 
   return (
     <View style={styles.container}>
-      <Text category="p1">{label}</Text>
+      <Text category="p1">
+        {label}
+        {required && <Text status={error ? 'danger' : 'basic'}>*</Text>}
+      </Text>
       <Controller
         control={control}
         name={name}
@@ -38,21 +45,26 @@ const FormInput: React.FC<FormInputProps> = ({
             onBlur={onBlur}
             onChangeText={onChange}
             value={value}
+            status={error ? 'danger' : 'basic'}
+            textStyle={error ? styles.redText : {}}
             {...rest}
             onSubmitEditing={handleKeyboardDismiss}
           />
         )}
         defaultValue=""
       />
-      {error && <Text>{error.message}</Text>}
+      {error && <Text status={error ? 'danger' : 'basic'}>{error.message}</Text>}
     </View>
   );
 };
 
 export default FormInput;
 
-const styles = StyleSheet.create({
+const themedStyles = StyleService.create({
   container: {
     gap: 4,
+  },
+  redText: {
+    color: '$color-danger-500',
   },
 });
--- mobile/src/screens/Login.tsx
+++ mobile/src/screens/Login.tsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
 import PageLayout from '../layouts/PageLayout';
-import { Icon } from '@ui-kitten/components';
+import { Icon, Text } from '@ui-kitten/components';
 import { useAuth } from '../hooks/useAuth';
 import { useTranslation } from 'react-i18next';
 import FormLayout from '../layouts/FormLayout';
 import FormInput from '../components/FormInput';
 import { useForm } from 'react-hook-form';
 import { TouchableWithoutFeedback } from 'react-native';
-import Paragraph from '../components/Paragraph';
 import * as yup from 'yup';
 import i18n from '../common/config/i18n';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -73,7 +72,7 @@ const Login = ({ navigation }: any) => {
       }}
     >
       <FormLayout>
-        <Paragraph>{`${t('paragraph')}`}</Paragraph>
+        <Text appearance="hint">{`${t('paragraph')}`}</Text>
         <FormInput
           control={control as any}
           name="username"
