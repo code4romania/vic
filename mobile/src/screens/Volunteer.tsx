aaf67d0f97424499b5ea299a28081d1f1c4ad512
Oncescu Laurentiu <lari.oncescu@wearetribus.com>
2023-05-15 14:35:34 +0300

    feat: add volunteer card

--- mobile/src/screens/Volunteer.tsx
+++ mobile/src/screens/Volunteer.tsx
@@ -3,6 +3,9 @@ import PageLayout from '../layouts/PageLayout';
 import { Button, Text } from '@ui-kitten/components';
 import NoVolunteerProfile from './NoVolunteerProfile';
 import i18n from '../common/config/i18n';
+import VolunteerCard from '../components/VolunteerCard';
+import { StyleSheet } from 'react-native';
+import { View } from 'react-native';
 
 const Volunteer = ({ navigation }: any) => {
   console.log('Volunteer', navigation);
@@ -25,11 +28,19 @@ const Volunteer = ({ navigation }: any) => {
 
   return true ? (
     <PageLayout title={i18n.t('tabs:volunteer')}>
-      <Text category="h1">Volunteer</Text>
-      <Button onPress={onShowDrawerPress}>SHOW DRAWER</Button>
-      <Button onPress={onViewOrganizationButtonPress}>View Organization</Button>
-      <Button onPress={onViewVolunteerProfilenButtonPress}>View Volunteer Profile</Button>
-      <Button onPress={onViewAtivityLogsButtonPress}>View Logs</Button>
+      <View style={styles.container}>
+        <Text>Volunteer</Text>
+        <Button onPress={onShowDrawerPress}>SHOW DRAWER</Button>
+        <Button onPress={onViewOrganizationButtonPress}>View Organization</Button>
+        <Button onPress={onViewVolunteerProfilenButtonPress}>View Volunteer Profile</Button>
+        <Button onPress={onViewAtivityLogsButtonPress}>View Logs</Button>
+        <VolunteerCard
+          title="Profil organizatie"
+          uri="https://picsum.photos/200/300"
+          onPress={() => console.log('volunteer card pressed')}
+          subtitle="2 Documente asteapta raspuns"
+        />
+      </View>
     </PageLayout>
   ) : (
     <NoVolunteerProfile />
@@ -37,3 +48,9 @@ const Volunteer = ({ navigation }: any) => {
 };
 
 export default Volunteer;
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 16,
+  },
+});
