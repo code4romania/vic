3c9c13fc859414380e905a70a430dbccc433107e
Oncescu Laurentiu <lari.oncescu@wearetribus.com>
2023-05-23 17:26:19 +0300

    feat: add activity logs list

--- mobile/src/screens/ActivityLogs.tsx
+++ mobile/src/screens/ActivityLogs.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
 import PageLayout from '../layouts/PageLayout';
-import { Button, Icon, StyleService, Text, useStyleSheet } from '@ui-kitten/components';
+import { Button, Divider, Icon, StyleService, Text, useStyleSheet } from '@ui-kitten/components';
 import i18n from '../common/config/i18n';
 import OrganizationIdentity from '../components/OrganizationIdentity';
 import Tabs from '../components/Tabs';
 import { ActivityLogsTabs } from '../common/constants/activity-logs-tabs';
-import { View } from 'react-native';
+import { View, VirtualizedList } from 'react-native';
+import LogItem from '../components/LogItem';
+
+interface ActivityLogItem {
+  id: string;
+  icon: string;
+  activityName: string;
+  date: string;
+  eventName: string;
+  hoursLogged: number;
+}
 
 const ActivityLogs = ({ navigation }: any) => {
   const styles = useStyleSheet(themedStyles);
   console.log('ActivityLogs');
 
+  const getItem = (): ActivityLogItem => ({
+    id: Math.random().toString(12).substring(0),
+    icon: 'heart',
+    activityName: 'Administrare documente de legatura',
+    date: '12.02.2022',
+    eventName: 'Haideti să sortam hainele verzi',
+    hoursLogged: 254,
+  });
+
+  const getItemCount = (_data: unknown) => 50;
+
   const onAddActivityLogButtonPress = () => {
     navigation.navigate('add-activity-log');
   };
 
-  const onViewActivityLogButtonPress = () => {
-    navigation.navigate('view-activity-log');
-  };
-
   const onTabPress = (id: string | number) => {
     console.log(id);
+  };
+
+  const onActivityLogPress = (id: string) => {
+    console.log(`activity log ${id} pressed`);
+    navigation.navigate('view-activity-log', { id });
   };
 
   return (
@@ -32,11 +54,25 @@ const ActivityLogs = ({ navigation }: any) => {
         <Text appearance="hint" style={styles.totalText}>
           {`${i18n.t('activity_log:total')}`} <Text category="p2">{`${235}h`} </Text>
         </Text>
+        <VirtualizedList
+          getItemCount={getItemCount}
+          renderItem={({ item }: { item: ActivityLogItem }) => (
+            <LogItem
+              icon={item.icon}
+              title={item.activityName}
+              date={item.date}
+              eventName={item.eventName}
+              duration={item.hoursLogged}
+              onPress={onActivityLogPress.bind(null, item.id)}
+            />
+          )}
+          getItem={getItem}
+          ItemSeparatorComponent={Divider}
+        />
       </Tabs>
       <Button onPress={onAddActivityLogButtonPress} style={styles.addButton}>
         {() => <Icon name="plus" style={styles.addIcon} />}
       </Button>
-      <Button onPress={onViewActivityLogButtonPress}>View</Button>
     </PageLayout>
   );
 };
