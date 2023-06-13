4e63d0533d62f7835155da6d035310c2942a7645
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2023-06-13 10:38:22 +0300

    chore:  update mobiel to 0.9.0

--- mobile/src/screens/Landing.tsx
+++ mobile/src/screens/Landing.tsx
@@ -57,7 +57,7 @@ const Landing = ({ navigation }: any) => {
         </View>
         <View style={styles.separator} />
         <LogInButton onPress={onLoginButtonPress} />
-        <Text style={styles.version}>v0.8</Text>
+        <Text style={styles.version}>v0.9</Text>
       </View>
     </PageLayout>
   );
