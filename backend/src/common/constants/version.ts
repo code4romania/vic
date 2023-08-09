51489807e1b1fd04e5d132ecc4047cf3ba539b0e
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2023-08-09 10:49:19 +0300

    chore: update versions

--- backend/src/common/constants/version.ts
+++ backend/src/common/constants/version.ts
@@ -1,1 +1,1 @@
-export const APP_VERSION = '0.17.7';
+export const APP_VERSION = '0.17.8';
--- mobile/src/screens/Landing.tsx
+++ mobile/src/screens/Landing.tsx
@@ -61,7 +61,7 @@ const Landing = ({ navigation }: any) => {
           <View style={styles.separator} />
           <LogInButton onPress={onLoginButtonPress} />
           <Text allowFontScaling={ALLOW_FONT_SCALLING} style={styles.version}>
-            v0.14.8
+            v0.14.9
           </Text>
         </View>
       </ScrollViewLayout>
