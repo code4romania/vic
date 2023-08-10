15cac10fdaddd8fcd8b47b8f4d93190eb2c29cfe
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2023-08-10 14:22:30 +0300

    chore: small update on login screen

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -5,9 +5,19 @@ const Login = () => {
   const { login } = useAuth();
 
   return (
-    <div>
-      This is login <button onClick={login}>Login</button>
-    </div>
+    <header className="bg-white">
+      <nav className="sm:px-10  py-4 px-4" aria-label="Top">
+        <div className="w-full flex gap-4 justify-between items-center">
+          <button
+            aria-label={'Login'}
+            className="bg-turquoise-700 sm:text-base text-sm sm:px-6 sm:py-2 px-2 py-1 shadow rounded-full text-white font-titilliumBold"
+            onClick={login}
+          >
+            Login
+          </button>
+        </div>
+      </nav>
+    </header>
   );
 };
 
