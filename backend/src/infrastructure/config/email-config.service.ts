7091448dbcdef0bc353e4ba078f83b04cfe25dd4
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2023-08-14 12:39:17 +0300

    fix: mail fix

--- backend/src/infrastructure/config/email-config.service.ts
+++ backend/src/infrastructure/config/email-config.service.ts
@@ -12,8 +12,8 @@ export class EmailConfigService {
       transport: {
         host: this.configService.get('MAIL_HOST'),
         port: +this.configService.get('MAIL_PORT'),
-        ignoreTLS: true,
-        secure: false,
+        ignoreTLS: false, // - if this is true and secure is false then TLS is not used even if the server supports STARTTLS extension
+        secure: false, // – if true the connection will use TLS when connecting to server. If false (the default) then TLS is used if server supports the STARTTLS extension. In most cases set this value to true if you are connecting to port 465. For port 587 or 25 keep it false
         auth: {
           user: this.configService.get('MAIL_USER'),
           pass: this.configService.get('MAIL_PASS'),
