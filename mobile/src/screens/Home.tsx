f2a9065f8361a77cbac5bcaff402f3f434e9b10f
luciatugui <lucia.tugui@wearetribus.com>
2024-07-22 14:47:03 +0300

    fix [#304]: add pull to refresh to home page

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Statistics from '../components/Statistics';
-import { StyleSheet, View } from 'react-native';
+import { Platform, RefreshControl, StyleSheet, View } from 'react-native';
 import { Layout, Text, useTheme } from '@ui-kitten/components';
 import LatestNews from '../components/LatestNews';
 import AboutVic from '../components/AboutVic';
@@ -13,6 +13,9 @@ import { useUserProfile } from '../store/profile/profile.selector';
 import { ALLOW_FONT_SCALLING } from '../common/constants/constants';
 import { Screen } from '../components/Screen';
 import { usePaddingTop } from '../hooks/usePaddingTop';
+import { useMonthlyStatistics, useVicStatistics } from '../services/statistics/statistics.service';
+import { getUserProfile } from '../services/user/user.api';
+import useStore from '../store/store';
 
 const Home = ({ navigation }: any) => {
   const { t } = useTranslation('home');
@@ -20,6 +23,12 @@ const Home = ({ navigation }: any) => {
   const paddingTop = usePaddingTop();
 
   const { userProfile } = useUserProfile();
+  const { setUserProfile } = useStore();
+
+  const { isRefetching: isRefetchingMonthlyStatistics, refetch: refetchMonthlyStatistics } =
+    useMonthlyStatistics();
+  const { isRefetching: isRefetchingVicStatistics, refetch: refetchVicStatistics } =
+    useVicStatistics();
 
   const onAddVolunteeringHours = () => {
     navigation.navigate('activity-logs');
@@ -29,71 +38,93 @@ const Home = ({ navigation }: any) => {
     navigation.navigate('search');
   };
 
-  return (
-    <Screen
-      preset="scroll"
-      statusBarStyle="light"
-      ScrollViewProps={{
-        bounces: false,
-        stickyHeaderIndices: [0],
-        showsVerticalScrollIndicator: false,
-      }}
-    >
-      <View style={styles.headerContainer}>
-        <View
-          style={{
-            ...styles.headerShape,
-            backgroundColor: theme['cool-gray-800'],
-            paddingTop,
-          }}
-        >
-          <Text allowFontScaling={ALLOW_FONT_SCALLING} style={styles.greetingText} category="h3">
-            {`${t('greeting', { name: userProfile?.firstName || '' })}`}{' '}
-            <IconSvg icon={WaveSvg} size={16} />
-          </Text>
-          <Paragraph style={{ color: theme['cool-gray-400'] }}>{`${t('paragraph')}`}</Paragraph>
-          {!userProfile?.activeOrganization && (
-            <>
-              <Text
-                allowFontScaling={ALLOW_FONT_SCALLING}
-                style={styles.greetingText}
-                category="h3"
-              >
-                {`${t('no_ngo_title')}`}
-              </Text>
-              <Paragraph style={{ color: theme['cool-gray-400'] }}>{`${t(
-                'no_ngo_paragraph',
-              )}`}</Paragraph>
-            </>
-          )}
+  const onRefresh = async () => {
+    refetchMonthlyStatistics();
+    refetchVicStatistics();
+    const profile = await getUserProfile();
+    setUserProfile(profile);
+  };
 
-          <View style={styles.addHoursContainer}>
-            {userProfile?.activeOrganization ? (
-              <Button
-                appearance="outline"
-                label={`${t('add_hours')}`}
-                style={styles.addButton}
-                onPress={onAddVolunteeringHours}
-              />
-            ) : (
-              <Button
-                appearance="outline"
-                label={`${t('join_ngo')}`}
-                style={styles.addButton}
-                onPress={onJoinOrganization}
-              />
+  return (
+    <>
+      <Screen
+        preset="scroll"
+        statusBarStyle="light"
+        ScrollViewProps={{
+          bounces: true,
+          // because on ios we add the backdrop for the refresh control, the actual sticky header will be at index 1
+          stickyHeaderIndices: Platform.OS === 'ios' ? [1] : [0],
+          showsVerticalScrollIndicator: false,
+          refreshControl: (
+            <RefreshControl
+              onRefresh={onRefresh}
+              refreshing={isRefetchingMonthlyStatistics || isRefetchingVicStatistics}
+              style={{ zIndex: 1 }}
+              tintColor="white"
+            />
+          ),
+        }}
+      >
+        {/* adds background color for the refresh control - only on iOS because on android the loader is on top of the screen */}
+        {Platform.OS === 'ios' && (
+          <View style={[styles.bounceBackdrop, { backgroundColor: theme['cool-gray-800'] }]} />
+        )}
+        <View style={styles.headerContainer}>
+          <View
+            style={{
+              ...styles.headerShape,
+              backgroundColor: theme['cool-gray-800'],
+              paddingTop,
+            }}
+          >
+            <Text allowFontScaling={ALLOW_FONT_SCALLING} style={styles.greetingText} category="h3">
+              {`${t('greeting', { name: userProfile?.firstName || '' })}`}{' '}
+              <IconSvg icon={WaveSvg} size={16} />
+            </Text>
+            <Paragraph style={{ color: theme['cool-gray-400'] }}>{`${t('paragraph')}`}</Paragraph>
+            {!userProfile?.activeOrganization && (
+              <>
+                <Text
+                  allowFontScaling={ALLOW_FONT_SCALLING}
+                  style={styles.greetingText}
+                  category="h3"
+                >
+                  {`${t('no_ngo_title')}`}
+                </Text>
+                <Paragraph style={{ color: theme['cool-gray-400'] }}>{`${t(
+                  'no_ngo_paragraph',
+                )}`}</Paragraph>
+              </>
             )}
+
+            <View style={styles.addHoursContainer}>
+              {userProfile?.activeOrganization ? (
+                <Button
+                  appearance="outline"
+                  label={`${t('add_hours')}`}
+                  style={styles.addButton}
+                  onPress={onAddVolunteeringHours}
+                />
+              ) : (
+                <Button
+                  appearance="outline"
+                  label={`${t('join_ngo')}`}
+                  style={styles.addButton}
+                  onPress={onJoinOrganization}
+                />
+              )}
+            </View>
           </View>
         </View>
-      </View>
-      <Layout style={styles.container}>
-        <Statistics navigation={navigation} />
-        <View style={styles.newsContainer}>
-          {userProfile?.activeOrganization && <LatestNews navigation={navigation} />}
-          <AboutVic />
-        </View>
-      </Layout>
-    </Screen>
+        <Layout style={styles.container}>
+          <Statistics navigation={navigation} />
+          <View style={styles.newsContainer}>
+            {userProfile?.activeOrganization && <LatestNews navigation={navigation} />}
+            <AboutVic />
+          </View>
+        </Layout>
+      </Screen>
+    </>
   );
 };
 
@@ -136,5 +167,12 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     gap: 32,
+  },
+  bounceBackdrop: {
+    height: 500,
+    position: 'absolute',
+    top: -500,
+    left: 0,
+    right: 0,
   },
 });
