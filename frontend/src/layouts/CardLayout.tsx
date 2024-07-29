3e25f1d097060dc0ca728257baefb27a8f6c4201
luciatugui <lucia.tugui@wearetribus.com>
2024-07-29 13:43:02 +0300

    fix: full height for profile card

--- frontend/src/layouts/CardLayout.tsx
+++ frontend/src/layouts/CardLayout.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { ComponentPropsWithoutRef } from 'react';
 
-interface CardProps {
+interface CardProps extends ComponentPropsWithoutRef<'div'> {
   children: React.ReactNode;
 }
 
-const Card = ({ children }: CardProps) => (
-  <div className="overflow-visible bg-white rounded-lg flex flex-col divide-y divide-cool-gray-200 gap-0 shadow-section grow">
+const Card = ({ children, className }: CardProps) => (
+  <div
+    className={`overflow-visible bg-white rounded-lg flex flex-col divide-y divide-cool-gray-200 gap-0 shadow-section grow ${className}`}
+  >
     {children}
   </div>
 );
