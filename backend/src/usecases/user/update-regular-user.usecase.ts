bd2b8cf23143b100f0bdabb67291cfe0b313361a
birloiflorian <birloiflorian@gmail.com>
2024-08-14 12:05:16 +0300

    fix: 232 - allow user to update phone number on registration screen (#361)
    Co-authored-by: Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>

--- backend/src/usecases/user/update-regular-user.usecase.ts
+++ backend/src/usecases/user/update-regular-user.usecase.ts
@@ -11,6 +11,7 @@ import { S3Service } from 'src/infrastructure/providers/s3/module/s3.service';
 import { S3_FILE_PATHS } from 'src/common/constants/constants';
 import { JSONStringifyError } from 'src/common/helpers/utils';
 import { GetOneRegularUserProfileUseCase } from './get-regule-user-profile.usecase';
+import { CognitoService } from 'src/infrastructure/providers/cognito/module/cognito.service';
 
 @Injectable()
 export class UpdateRegularUserUsecase
@@ -23,6 +24,7 @@ export class UpdateRegularUserUsecase
     private readonly getRegularUserProfileUsecase: GetOneRegularUserProfileUseCase,
     private readonly exceptionService: ExceptionsService,
     private readonly s3Service: S3Service,
+    private readonly cognitoService: CognitoService,
   ) {}
 
   async execute(
@@ -38,7 +40,22 @@ export class UpdateRegularUserUsecase
       this.exceptionService.notFoundException(UserExceptionMessages.USER_001);
     }
 
-    // 3. check if new profilePicture is provided
+    // 3. Check if the user phone number is the same as the one from the request
+    if (user.phone !== userUpdates.phone) {
+      try {
+        await this.cognitoService.updateUser(user.cognitoId, userUpdates.phone);
+      } catch (error) {
+        this.logger.error({
+          ...UserExceptionMessages.USER_008,
+          error: JSONStringifyError(error),
+        });
+        this.exceptionService.internalServerErrorException(
+          UserExceptionMessages.USER_008,
+        );
+      }
+    }
+
+    // 4. check if new profilePicture is provided
     if (profilePicture) {
       try {
         // 3.1 upload File and get path
@@ -67,13 +84,13 @@ export class UpdateRegularUserUsecase
       }
     }
 
-    // 4 update db structure
+    // 5 update db structure
     const updatedUser = await this.userService.updateRegularUser(
       id,
       userUpdates,
     );
 
-    // 5. return user profile with presigned url
+    // 6. return user profile with presigned url
     return this.getRegularUserProfileUsecase.execute({ id: updatedUser.id });
   }
 }
