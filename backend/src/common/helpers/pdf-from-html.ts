afbb42921768e673adeee477316014c95221a3c1
Andrew Radulescu <radulescuandrew@gmail.com>
2024-08-29 10:42:28 +0300

    feat: [Contracts] Configure puppeteer to run headless

--- backend/src/common/helpers/pdf-from-html.ts
+++ backend/src/common/helpers/pdf-from-html.ts
@@ -1,7 +1,10 @@
 import puppeteer from 'puppeteer';
 
 export const HTMLtoPDF = async (html: string): Promise<Uint8Array> => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
   await page.setContent(html);
   const buffer = await page.pdf({ format: 'A4' });
