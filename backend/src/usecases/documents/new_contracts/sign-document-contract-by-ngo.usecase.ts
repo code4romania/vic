a9d52d7036dbccf791c8ef0fac285f652be33525
Andrew Radulescu <radulescuandrew@gmail.com>
2024-09-23 17:44:43 +0300

    fix: [Contracts] Add signature to signContractByNGO endpoint

--- backend/src/usecases/documents/new_contracts/sign-document-contract-by-ngo.usecase.ts
+++ backend/src/usecases/documents/new_contracts/sign-document-contract-by-ngo.usecase.ts
@@ -4,21 +4,25 @@ import { ExceptionsService } from 'src/infrastructure/exceptions/exceptions.serv
 import { DocumentContractStatus } from 'src/modules/documents/enums/contract-status.enum';
 import { ContractExceptionMessages } from 'src/modules/documents/exceptions/contract.exceptions';
 import { DocumentContractFacade } from 'src/modules/documents/services/document-contract.facade';
+import { DocumentSignatureFacade } from 'src/modules/documents/services/document-signature.facade';
+import { IAdminUserModel } from 'src/modules/user/models/admin-user.model';
 
 @Injectable()
 export class SignDocumentContractByNgoUsecase implements IUseCaseService<void> {
   constructor(
     private readonly documentContractFacade: DocumentContractFacade,
+    private readonly documentSignatureFacade: DocumentSignatureFacade,
     private readonly exceptionService: ExceptionsService,
   ) {}
 
   public async execute(
     documentContractId: string,
-    organizationId: string,
+    signatureBase64: string,
+    admin: IAdminUserModel,
   ): Promise<void> {
     const exists = await this.documentContractFacade.exists({
       id: documentContractId,
-      organizationId,
+      organizationId: admin.organizationId,
       status: DocumentContractStatus.PENDING_NGO_REPRESENTATIVE_SIGNATURE,
     });
 
@@ -29,8 +33,14 @@ export class SignDocumentContractByNgoUsecase implements IUseCaseService<void> {
     }
 
     try {
+      const signatureId = await this.documentSignatureFacade.create({
+        userId: admin.id,
+        signature: signatureBase64,
+      });
+
       await this.documentContractFacade.signDocumentContractByNGO(
         documentContractId,
+        signatureId,
       );
     } catch (error) {
       // TODO: Update error
